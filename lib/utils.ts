d478e20a4d394a57eca217425b1452c3b4ce7047
devMukulSingh <mukulsingh2276@gmail.com>
2024-10-16 13:56:29 +0530

    updated baseurl for prod

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,7 +5,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const base_url_server = `http://localhost:8000/api/v1`;
+export const base_url_server = process.env.NODE_ENV === 'production' ? 'https://dashboardserver-z9yo.onrender.com' : `http://localhost:8000/api/v1`;
 
 export const fetcher = async ([url, key, token]: [
   url: string,
